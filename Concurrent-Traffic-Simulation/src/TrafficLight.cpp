c7b31d0018349117c5ead4a97b83bf7b4c8e0979
Prateek <prana1997@gmail.com>
2022-11-28 21:31:54 +0000

    finishing task 2b

--- Concurrent-Traffic-Simulation/src/TrafficLight.cpp
+++ Concurrent-Traffic-Simulation/src/TrafficLight.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include "TrafficLight.h"
 #include <cstdlib>
+#include <thread>
 /* Implementation of class "MessageQueue" */
 
 
@@ -44,8 +45,8 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-  
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+  	threads.emplace_back(std::thread(cycleThroughPhases()));
+    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method "simulate" is called. To do this, use the thread queue in the base class. 
   
 }
 
